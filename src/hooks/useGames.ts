9326fc7ada97720a537f3d581ec0b2757df5d4f1
Yode <yode0419@gmail.com>
2023-10-11 14:54:07 +0800

    Build game cards

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -3,9 +3,10 @@ import apiClient from "../services/api-client";
 import { spring } from "framer-motion";
 import { CanceledError } from "axios";
 
-interface Game {
+export interface Game {
   id: number;
   name: string;
+  background_image: string;
 }
 
 interface FetchGamesResponse {
