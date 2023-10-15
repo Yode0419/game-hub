f3e52fdffdcfdd45f5ae29f60fecd3298095d840
Yode <yode0419@gmail.com>
2023-10-15 15:19:15 +0800

    Debug: filter games by genre

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -21,17 +21,17 @@ const GameGrid = ({ selectedGenre }: Props) => {
         spacing={3}
         padding={"10px"}
       >
-        {isLoading &&
-          skeletons.map((skeleton) => (
-            <GameCardContainer key={skeleton}>
-              <GameCardSkeleton />
-            </GameCardContainer>
-          ))}
-        {data.map((game) => (
-          <GameCardContainer key={game.id}>
-            <GameCard game={game} />
-          </GameCardContainer>
-        ))}
+        {isLoading
+          ? skeletons.map((skeleton) => (
+              <GameCardContainer key={skeleton}>
+                <GameCardSkeleton />
+              </GameCardContainer>
+            ))
+          : data.map((game) => (
+              <GameCardContainer key={game.id}>
+                <GameCard game={game} />
+              </GameCardContainer>
+            ))}
       </SimpleGrid>
     </>
   );
